5587b28b425db4fc73eb0e5f28ebf5dae7cb2952
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2019-06-23 23:26:35 +1000

    Avoid key sorting

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import json
 import yaml
 import sys
 
-print(yaml.dump(json.load(sys.stdin), default_flow_style=False))
+print(yaml.dump(
+    json.load(sys.stdin), 
+    default_flow_style=False,
+    sort_keys=False
+))
